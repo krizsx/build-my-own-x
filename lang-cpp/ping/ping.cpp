898e25f39dbb288bd94ea1b74b3f2b71416e7814
krizsx <cb.kris.bj@gmail.com>
2020-12-06 18:22:20 +0800

    :sparkles: add devcontainer config

---
+++ lang-cpp/ping/ping.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file ping.cpp
+ * @author krizsx (cb.kris.bj@gmail.com)
+ * @brief ping command
+ * @version 0.1
+ * @date 2020-12-06
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
